c0528c6351a1574968a5a77364620faa82908b6b
Stephen Joyner <stephen@rundeck.com>
2022-10-03 16:13:06 -0500

    Fix for api lib updates

--- src/main/java/com/rundeck/feature/systemactions/actions/DispatchSystemMessageFeatureAction.java
+++ src/main/java/com/rundeck/feature/systemactions/actions/DispatchSystemMessageFeatureAction.java
@@ -1,13 +1,12 @@
 package com.rundeck.feature.systemactions.actions;
 
 import com.rundeck.feature.api.action.FeatureAction;
-import com.rundeck.feature.api.context.ContextKey;
+import com.rundeck.feature.api.context.ContextKeys;
 import com.rundeck.feature.api.context.FeatureActionContext;
 import com.rundeck.feature.api.model.CompletionStatus;
 import com.rundeck.feature.api.output.OutputLevel;
 import com.rundeck.feature.systemactions.events.LogActionOutputEvent;
 import com.rundeck.feature.systemactions.events.SystemMessageActionEvent;
-import org.springframework.context.ApplicationContext;
 
 public class DispatchSystemMessageFeatureAction implements FeatureAction<DispatchSystemMessageFeatureActionData> {
     public static final String NAME = "dispatch-message";
@@ -28,7 +27,7 @@ public class DispatchSystemMessageFeatureAction implements FeatureAction<Dispatc
         var evtPublisher = featureActionContext.getEventPublisher();
         String aid = featureActionContext.getActionId();
         try {
-            var data = featureActionContext.get(ContextKey.ACTION_DATA, DispatchSystemMessageFeatureActionData.class);
+            var data = featureActionContext.get(ContextKeys.ACTION_DATA, DispatchSystemMessageFeatureActionData.class);
 
             featureActionContext.getEventPublisher().publish(data.topic, new SystemMessageActionEvent(aid,System.nanoTime(), data.getPayload(), data.getAsUser(), featureActionContext.getUser()));
 
